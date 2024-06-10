6c367c32f74a5503f2edb5e80f4e3242a70dcad3
Puseletso Chuene <puseletsochuene88@gmail.com>
2024-06-10 22:26:53 +0200

    variables exercise complete

--- variables.js
+++ variables.js
@@ -1,4 +1,33 @@
+// VARIABLES
+// Excercise 1
+
 let name = 'Puseletso'
 let age = '22'
 let isStudent = false
-console.log(name, age, isStudent)
+
+console.log(name)
+console.log(age)
+console.log(isStudent)
+
+// Exercise 2
+
+let favoriteColour = 'Black'
+favoriteColour = 'Navy Blue'
+console.log(favoriteColour)
+
+// Exercise 3
+
+let num1 = 19
+let num2 = 12
+
+// Sum, difference, product, quotient
+let sum = num1 + num2
+let difference = num1 - num2
+let product = num1 * num2
+let quotient = num1 / num2
+
+console.log(sum)
+console.log(difference)
+console.log(product)
+console.log(quotient)
+
